1044cae7a7a459c28f7fc0cc9312a3e94e1b8ac0
mickleon <mickleon64@gmail.com>
2025-12-31 13:49:29 +0400

    feat: add floating term

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      size = 10,
+      open_mapping = {},
+      hide_numbers = true,
+      shade_terminals = true,
+      start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      persist_size = true,
+      direction = "float",
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = {
+          border = "Normal",
+          background = "Normal",
+        },
+      },
+    },
+  },
+}
