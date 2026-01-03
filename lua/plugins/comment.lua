e76fda4707ac6cce738abb5800707b8b490fc98e
mickleon <mickleon64@gmail.com>
2026-01-03 21:52:17 +0400

    feat: add coment keymaps

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,3 +1,21 @@
 return {
   "numToStr/Comment.nvim",
+  opts = {
+    toggler = {
+        line = 'gcc',
+    },
+    opleader = {
+        line = 'gc',
+        block = 'gb',
+    },
+    extra = {
+        above = 'gcO',
+        below = 'gco',
+        eol = 'gcA',
+    },
+    mappings = {
+        basic = true,
+        extra = true,
+    },
+  }
 }
