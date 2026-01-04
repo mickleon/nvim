74b1eca99a6a819cb079b58bb237e0834ce4dab2
mickleon <mickleon64@gmail.com>
2026-01-04 20:09:24 +0400

    feat: improve colors

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,22 +4,16 @@ return {
     opts = {
       size = 10,
       open_mapping = [[<C-\>]],
-      hide_numbers = true,
-      shade_terminals = true,
-      start_in_insert = true,
-      insert_mappings = true,
-      terminal_mappings = true,
       persist_size = true,
       direction = "float",
-      close_on_exit = true,
       shell = vim.o.shell,
+      highlights = {
+        FloatBorder = {
+          link = "FloatBorder",
+        },
+      },
       float_opts = {
         border = "curved",
-        winblend = 0,
-        highlights = {
-          border = "Normal",
-          background = "Normal",
-        },
         width = 80,
         height = 20,
       },
