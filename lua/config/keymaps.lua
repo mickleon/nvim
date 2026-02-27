11dfbefd00d276dce5c156011b2c74daa6ab511e
mickleon <mickleon64@gmail.com>
2026-02-27 20:08:17 +0400

    feat: add some ru keyboard analog keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- local map = LazyVim.safe_keymap_set
+local map = LazyVim.safe_keymap_set
+
+map("n", "ш", "i")
+map("n", "м", "v")
+map("n", "в", "d")
+map("n", "вв", "dd")
+map("n", "г", "u")
+map("n", "<C-к>", "<C-r>")
+map("n", "н", "y")
+map("n", "Жц", "<cmd>w<cr>")
+map("n", "Жй", "<cmd>q<cr>")
+map({ "n", "i" }, "<C-ы>", "<cmd>w<cr>")
